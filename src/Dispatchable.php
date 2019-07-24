3111a4efa4030e86c8529afc211120039f77b7ba
Peter Klooster <peter.klooster@webparking.nl>
2019-07-24 16:36:15 +0200

    Added Dispatchable & PendingDispatch classes

---
+++ src/Dispatchable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Imtigger\LaravelJobStatus;
+
+use Illuminate\Contracts\Bus\Dispatcher;
+use Illuminate\Foundation\Bus\PendingChain;
+
+trait Dispatchable
+{
+    /**
+     * Dispatch the job with the given arguments.
+     *
+     * @return \Illuminate\Foundation\Bus\PendingDispatch
+     */
+    public static function dispatch()
+    {
+        return new PendingDispatch(new static(...\func_get_args()));
+    }
+
+    /**
+     * Dispatch a command to its appropriate handler in the current process.
+     *
+     * @return mixed
+     */
+    public static function dispatchNow()
+    {
+        return app(Dispatcher::class)->dispatchNow(new static(...\func_get_args()));
+    }
+
+    /**
+     * Set the jobs that should run if this job is successful.
+     *
+     * @param  array                                   $chain
+     * @return \Illuminate\Foundation\Bus\PendingChain
+     */
+    public static function withChain($chain)
+    {
+        return new PendingChain(static::class, $chain);
+    }
+}
